22d84592b4945f3182f8af49a0b73bee76125f23
Parker Roan <parker@fopark.com>
2018-03-29 19:35:06 -0500

    Initial Commit

---
+++ text_monitor.py
@@ -0,0 +1,284 @@
+from flask import Flask, request, jsonify, url_for
+import requests
+from bandwidth import messaging
+from trading_view_bot import gdax_bot
+import gdax
+import json
+import datetime
+import pytz
+import time
+from decimal import *
+import signal
+import sys
+import settings
+
+
+app = Flask(__name__)
+auth_client = gdax.AuthenticatedClient(settings.GDAX_API_KEY, settings.GDAX_PRIVATE_KEY, settings.GDAX_PASSPHRASE)
+
+def sigint_handler(signum, frame):
+	bot_dict = app.config.get('GDAX_BOT_DICT')
+	if bot_dict:
+		for key,bot in bot_dict.items(): 
+			bot.orderbook.close()
+			print "closing orderbook"
+	time.sleep(1)
+	sys.exit()
+ 
+signal.signal(signal.SIGINT, sigint_handler)
+
+def create_app(gdax_bot_dict):
+	app.config['GDAX_BOT_DICT'] = gdax_bot_dict
+	app.config['LOG_PATH'] = "order-log.json"
+	app.config['STOP_FLAG'] = False
+	app.config['STOP_LIMIT'] = .92
+	app.config['MESSAGE_API'] = messaging.Client(settings.BANDWIDTH_USER, settings.BANDWIDTH_TOKEN, settings.BANDWIDTH_SECRET)
+	app.config['ROOT_PHONE'] = settings.ROOT_NUMBER
+	app.config['INIT_EQUIV_FIAT'] = equivalent_fiat(app.config['GDAX_BOT_DICT'])
+	print "INIT FIAT SUM: {}".format(app.config['INIT_EQUIV_FIAT'])
+	return app
+
+def text_message(message_api,message_body):
+
+	message_id = message_api.send_message(from_ = '+1{}'.format(settings.ORIGIN_NUMBER),
+                              to = '+1{}'.format(settings.ROOT_NUMBER),
+                              text = message_body)
+
+def equivalent_fiat(bot_dict):
+	equiv_fiat_sum = Decimal(0.00)
+	fiat_balance = Decimal(0.00)
+	for coin_id,bot in bot_dict.items(): 
+		price = bot.get_price()
+		if price: 
+			time.sleep(.2)
+			coin_balance,fiat_balance = bot.get_balances(pending_flag = False)
+			equiv_fiat_sum += Decimal(coin_balance * price).quantize(Decimal('.01'), rounding=ROUND_DOWN)
+			fiat_balance = fiat_balance
+			time.sleep(.5)
+		else: 
+			return None
+
+	total_sum = fiat_balance + equiv_fiat_sum
+	
+	return round(total_sum,2)
+
+def logger(log_path,log_message):
+	utc_now = pytz.utc.localize(datetime.datetime.utcnow())
+	#pst_now = utc_now.astimezone(pytz.timezone("America/Los_Angeles"))
+	ct_now = utc_now.astimezone(pytz.timezone("America/Chicago"))
+	log_message['central-timezone'] = str(ct_now.isoformat())
+	
+	with open(log_path,'a+') as f:
+		json.dump(log_message, f)
+		f.write('\n')
+
+def check_message(message_id):
+	message_id = message_id.replace('<','')
+	ret =  requests.get(
+		"{}/events".format(settings.MAILGUN_API_URL,
+		auth=("api", settings.MAILGUN_API_KEY),
+		params={"message-id": message_id}))
+	#Check if return was valid if not then do not place trade
+	print "mailgun status code: {}".format(ret.status_code)
+	if ret.status_code == 200:
+		data =  ret.json()
+
+		for event in  data.get('items'):
+			if event.get('delivery-status'):
+				if event.get('delivery-status').get('attempt-no') > 1:
+					print event.get('delivery-status')
+					return False
+		return True
+
+	return True
+
+@app.route('/sms', methods=['POST'])
+def sms():
+	app.config['ROOT_FLAG'] = False
+	
+	data =  request.data
+	data = json.loads(data)
+	message_body = data.get('text').lower()
+	number = data.get('from')
+	root_number = app.config['ROOT_PHONE']
+	message_api = app.config['MESSAGE_API']
+
+
+	if root_number in number:
+		print "HELLO PARKER"
+	else: 
+		return "NOT AUTHORIZED", 403
+
+	bot_dict = app.config['GDAX_BOT_DICT']
+	print message_body
+
+	if "shutdown" in message_body or "stop" in message_body:
+		print "SHUTTING DOWN BOT"
+		text_message(message_api,"SHUTTING DOWN BOT") 
+		app.config['STOP_FLAG'] = True
+
+	if "shutdown" in message_body and "sell" in message_body.lower():
+		print "SHUTTING DOWN AND SELLING ALL"
+		text_message(message_api,"SHUTTING DOWN AND SELLING ALL") 
+		app.config['STOP_FLAG'] = True
+		for key,value in gdax_bot_dict.items():
+				bot = value
+				bot.run(short_sell_flag = True)
+
+	if "resume" in message_body: 
+		print "RESUMING BOT"
+		text_message(message_api,"RESUMING BOT") 
+		app.config['STOP_FLAG'] = False
+
+	if "reset" in message_body and "init" in message_body and "fiat" in message_body: 
+		app.config['INIT_EQUIV_FIAT'] = equivalent_fiat()
+		if "resume" in message_body:
+			app.config['STOP_FLAG'] = False
+
+	if "status" in message_body: 
+		total_fiat = equivalent_fiat(bot_dict)
+		text = "Init_Fiat_Balance: {} \nEquiv_Fiat_Balance: {} \n Stop_Flag: {} \n\n\n".format(app.config['INIT_EQUIV_FIAT'],total_fiat,app.config['STOP_FLAG'])
+		for coin_id,bot in bot_dict.items():
+			equiv_fiat,coin_balance = bot.get_equivalent_fiat()
+			text += 'Coin_ID: {} \n Price: {} \n Coin_Balance_Value: {} \n Coin_Balance: {} \n Short_Flag: {} \n Long_Flag: {} \n\n'.format(bot.coin_id,bot.get_price(),equiv_fiat,coin_balance,bot.short_flag,bot.long_flag)
+		text_message(message_api,text)
+
+	if "short" in message_body and "buy" in message_body:
+		for key,bot in bot_dict.items(): 
+			if key in message_body:
+				bot.run(short_buy_flag = True)
+
+	if "short" in message_body and "sell" in message_body:
+		for key,bot in bot_dict.items(): 
+			if key in message_body:
+				bot.run(short_sell_flag = True)
+
+	if "long" in message_body and "buy" in message_body:
+		for key,bot in bot_dict.items(): 
+			if key in message_body:
+				bot.run(long_buy_flag = True)
+
+	if "long" in message_body and "sell" in message_body:
+		for key,bot in bot_dict.items(): 
+			if key in message_body:
+				bot.run(long_sell_flag = True)
+
+	if "long" in message_body and "flag":
+		for key,bot in bot_dict.items(): 
+			if key in message_body:
+				if "true" in message_body:
+					bot.long_flag = True
+				elif "false" in message_body:
+					bot.long_flag = False
+
+	if "short" in message_body and "flag":
+		for key,bot in bot_dict.items(): 
+			if key in message_body:
+				if "true" in message_body:
+					bot.short_flag = True
+				elif "false" in message_body:
+					bot.short_flag = False
+
+	return str(message_body)
+
+@app.route('/email', methods=['POST'])
+def email():
+	message_id = request.form.get('Message-Id')
+	print message_id
+	message_body =  request.form.get('subject').lower()
+	
+	if not check_message(message_id):
+		return jsonify({'message':'Email was a retry and was discarded'})
+
+	bot_dict = app.config['GDAX_BOT_DICT']
+	log_path = app.config['LOG_PATH']
+	message_api = app.config['MESSAGE_API']
+	print message_body
+
+	STOP_FLAG = app.config['STOP_FLAG']
+	equiv_fiat = None
+	init_fiat = None
+
+	stop_flag = stop_check()
+
+	if stop_flag:
+		app.config['STOP_FLAG'] = True
+
+	STOP_FLAG = app.config['STOP_FLAG']
+
+
+	if "short" in message_body and "buy" in message_body:
+		if not STOP_FLAG:
+			for key,bot in bot_dict.items(): 
+				if key in message_body:
+					equiv_fiat,coin_balance,fiat_balance,price,buy_flag,sell_flag = bot.run(short_buy_flag = True)
+					log_text = {"price":float(price),"coin_id":key,"message":message_body,"method":"short buy"}
+					
+					if buy_flag and bot.pending_order:
+						logger(log_path,log_text)
+						text_message(message_api,str(log_text))
+
+	if "short" in message_body and "sell" in message_body:
+		if not STOP_FLAG:
+			for key,bot in bot_dict.items(): 
+				if key in message_body:
+					equiv_fiat,coin_balance,fiat_balance,price,buy_flag,sell_flag = bot.run(short_sell_flag = True)
+					log_text = {"price":float(price),"coin_id":key,"message":message_body,"method":"short sell"}
+					
+					if sell_flag and bot.pending_order:
+						logger(log_path,log_text)
+						text_message(message_api,str(log_text))
+
+	if "long" in message_body and "buy" in message_body:
+		if not STOP_FLAG:
+			for key,bot in bot_dict.items(): 
+				if key in message_body:
+					equiv_fiat,coin_balance,fiat_balance,price,buy_flag,sell_flag = bot.run(long_buy_flag = True)
+					log_text = {"price":float(price),"coin_id":key,"message":message_body,"method":"long buy"}
+					
+					if buy_flag and bot.pending_order:
+						logger(log_path,log_text)
+						text_message(message_api,str(log_text))
+
+	if "long" in message_body and "sell" in message_body:
+		if not STOP_FLAG:
+			for key,bot in bot_dict.items(): 
+				if key in message_body:
+					equiv_fiat,coin_balance,fiat_balance,price,buy_flag,sell_flag = bot.run(long_sell_flag = True)
+					log_text = {"price":float(price),"coin_id":key,"message":message_body,"method":"long sell"}
+					
+					if sell_flag and bot.pending_order:
+						logger(log_path,log_text)
+						text_message(message_api,str(log_text))
+
+	return str(message_body)
+
+def stop_check():
+	with app.app_context():
+		print "Check equivalent balance"
+		bot_dict = app.config['GDAX_BOT_DICT']
+		init_fiat = app.config['INIT_EQUIV_FIAT']
+		total_fiat = equivalent_fiat(bot_dict)
+		stop_limit = app.config['STOP_LIMIT']
+
+		if total_fiat: 
+			if total_fiat < init_fiat * stop_limit: 
+				app.config['STOP_FLAG'] = True
+				text = 'GDAX Bot initiated stop limit Equivilent_Fiat:{} Init_Fiat:{}'.format(equiv_fiat,init_fiat)
+				message_api = app.config['MESSAGE_API']
+				text_message(message_api,text)
+				print text
+				return True
+		return False
+
+if __name__ == '__main__':
+	coin_ids = ["LTC","ETH","BTC"]
+	gdax_bot_dict = {}
+	
+	for coin_id in coin_ids: 
+		product_id = coin_id + "-USD"
+		gdax_bot_dict[coin_id.lower()] = gdax_bot(coin_id,product_id,auth_client)
+
+	app = create_app(gdax_bot_dict)
+
+	app.run(host='0.0.0.0')
---
+++ trading_view_bot.py
@@ -0,0 +1,396 @@
+import gdax 
+import time
+import os
+import numpy as np
+import datetime
+import dateutil.parser 
+from dateutil.tz import *
+import math
+from scipy import stats
+from decimal import *
+import threading
+import Queue as queue
+
+class gdax_bot(): 
+	def __init__ (self,coin_id,product_id,auth_client):
+		print "Initializing GDAX Bot PRODUCT: {}".format(product_id)
+
+		self.auth_client = auth_client
+		self.pc = gdax.PublicClient()
+		self.coin_id = coin_id
+		self.product_id = product_id
+		self.orderbook = gdax.OrderBook(product_id = [self.product_id])
+		self.init_orderbook()
+		
+		self.min_amount, self.quote_increment, self.min_market_funds = self.get_product_info()
+		self.last_buy_price = None
+
+		self.short_max_profit = 1.03
+		self.long_max_profit = 1.06
+		self.max_loss = .97
+		self.max_slippage = .002
+
+		self.equivalent_fiat = None
+
+		self.long_flag = False
+		self.short_flag = False
+
+		self.open_orders = []
+		self.order_thread = None
+		self.pending_order = False
+		self.order_exp = 10 #sec Time until bot should cancel limit order and create new one
+		self.get_orders()
+
+	def init_orderbook(self):
+		self.orderbook.start()
+		ready = False
+		while not ready:
+			try:
+				print self.orderbook.get_ask()
+				ready = True
+				print "ORDERBOOK INITIALIZED"
+			except:
+				time.sleep(1)
+
+	def get_product_info(self):
+		ret = self.pc.get_products()
+		min_amount = 0
+		product_id_list = self.product_id.split("-")
+
+		for product in ret: 
+			if "{}/{}".format(product_id_list[0],product_id_list[1]) == product.get('display_name'):
+				min_amount = product.get("base_min_size")
+				quote_increment = product.get("quote_increment")
+				min_market_funds = product.get("min_market_funds")
+		return Decimal(min_amount), Decimal(quote_increment), Decimal(min_market_funds)
+
+
+	def check_order(self,order_id):
+		if order_id: 
+			ret = self.auth_client.get_order(order_id)
+			return ret
+
+	def get_orders(self):
+		order_generator = self.auth_client.get_orders()
+		orders = list(order_generator)
+		return orders
+
+	def stop_limit(self,buy_price,current_price,coin_balance):
+		if buy_price and coin_balance > self.min_amount: 
+			if self.short_flag:
+				if current_price > (buy_price*self.short_max_profit) or current_price < (buy_price*self.max_loss):
+					ret = self.place_sell(current_price,coin_balance)
+					
+					if ret: 
+						self.long_flag = False
+						self.short_flag = False 
+					print ret
+
+			elif self.long_flag:
+				if current_price > (buy_price*self.short_max_profit) or current_price < (buy_price*self.max_loss):
+					ret = self.place_sell(current_price,coin_balance)
+					
+					if ret: 
+						self.long_flag = False
+						self.short_flag = False 
+					print ret
+
+	def round_fiat(self, money):
+		return Decimal(money).quantize(Decimal('.01'), rounding=ROUND_DOWN)
+
+	def round_coin(self, money):
+		return Decimal(money).quantize(Decimal('.00000001'), rounding=ROUND_DOWN)
+
+
+	def get_balances(self,pending_flag = True):
+		accounts = self.auth_client.get_accounts()
+		for account in accounts: 
+			if account.get('currency') == 'USD':
+				fiat_balance = self.round_fiat(account.get('balance'))
+			if account.get('currency') == self.coin_id:
+				coin_balance = self.round_coin(account.get('balance'))
+		
+		if pending_flag:
+			pending_order_sum = Decimal(0.00)
+			pending_sell_sum = Decimal(0.00000000)
+			orders = self.get_orders()
+			for order in orders: 
+				if order.get('side') == 'buy':
+					coin_size = Decimal(order.get('size'))
+					price = Decimal(order.get('price'))
+					pending_order_sum += self.round_fiat(coin_size * price)
+				
+				if order.get('side') == 'sell':
+					coin_size = Decimal(order.get('size'))
+					pending_sell_sum += coin_size
+
+			fiat_balance = fiat_balance - pending_order_sum
+			coin_balance = coin_balance - pending_sell_sum
+		
+		return coin_balance,fiat_balance	
+
+
+	def get_equivalent_fiat(self):
+		coin_balance,fiat_balance = self.get_balances()
+		if self.equivalent_fiat is None: 
+			self.run()
+		return self.round_fiat(self.equivalent_fiat), coin_balance
+
+	def get_ask(self):
+		try:
+			buy_price = self.orderbook.get_ask()
+			return Decimal(buy_price)
+		except:
+			self.orderbook.close()
+			time.sleep(1)
+			self.init_orderbook()
+			buy_price = self.orderbook.get_ask()
+			return Decimal(buy_price)
+
+	def get_bid(self):
+		try:
+			sell_price = self.orderbook.get_bid()
+			return Decimal(sell_price)
+		except:
+			self.orderbook.close()
+			time.sleep(1)
+			self.init_orderbook()
+			sell_price = self.orderbook.get_ask()
+			return Decimal(sell_price)
+
+	def buy(self,price = None,size = None,buy_type = 'limit',partial = 1.0):
+		if size < self.min_amount:
+			return {'status':'done'}
+
+		if buy_type is 'limit' and price and size:
+			amount = self.round_coin(size * Decimal(partial))
+			if amount > self.min_amount:
+				size = amount
+			ret = self.auth_client.buy(price= str(price), size=str(size), product_id=self.product_id, post_only = True, order_type = "limit")
+
+		else:
+			ret = self.auth_client.buy(funds= str(self.get_balances()[1]), product_id=self.product_id, order_type = "market")
+
+		return ret
+
+	def place_buy(self):
+		coin_balance,fiat_balance = self.get_balances()
+		order_flag = False
+
+		try: 
+			auth_ret = self.auth_client.cancel_all(product_id=self.product_id)
+			initial_price = self.get_ask() - self.quote_increment
+			size = self.round_coin(fiat_balance/initial_price)
+			if size >= self.min_amount:
+				order_flag = True
+				buy_price = initial_price
+				self.pending_order = True
+				print "Starting Buy Thread"
+				while getattr(self.order_thread,"run", True) and self.buy_flag and size > self.min_amount or len(self.open_orders) > 0:
+					
+					#Check for max slippage and if so do a market buy
+					if (1 - buy_price/initial_price) >= self.max_slippage:
+						self.auth_client.cancel_all(product_id=self.product_id)
+						self.buy(buy_type = 'market')
+						self.pending_order = False
+						return True
+
+					if len(self.open_orders) > 0: 
+						ret = self.buy(price = buy_price,size = size,buy_type = "limit",partial = 1.0)
+					else: 
+						ret = self.buy(price = buy_price,size = size,buy_type = "limit",partial = .5)
+					
+	               	#Check each order for completion or expiration of 60+ sec
+					self.open_orders = self.get_orders()
+					for order in self.open_orders:
+						order_time = dateutil.parser.parse(order.get('created_at'))
+						dt = datetime.datetime.utcnow().replace(tzinfo = tzutc()) - order_time
+						if dt.total_seconds() > 60: 
+							self.auth_client.cancel_order(order.get('id'))
+
+					time.sleep(2)
+
+					buy_price = self.get_ask() - self.quote_increment
+					coin_balance,fiat_balance = self.get_balances()
+					size = self.round_coin(fiat_balance/buy_price)
+
+		except Exception as err: 
+			print "Error in place_buy: {}".format(err)
+	       	#Check each order for completion or expiration of 10+ sec
+		auth_ret = self.auth_client.cancel_all(product_id=self.product_id)
+		self.open_orders = self.get_orders()
+		self.pending_order = False
+		print "Exiting Buy Thread"
+		return order_flag
+
+	def sell(self,price = None,size = None,sell_type = 'limit',partial = 1.0):
+		if size < self.min_amount:
+			return {'status':'done'}
+
+		if sell_type is 'limit' and price and size:
+			amount = self.round_coin(size * Decimal(partial))
+			if amount > self.min_amount:
+				size = amount
+			ret = self.auth_client.sell(price= str(price), size=str(size), product_id=self.product_id, post_only = True, order_type = "limit")
+
+		elif sell_type == 'market':
+			ret = self.auth_client.sell(size= size, product_id=self.product_id, order_type = "market")
+		return ret
+
+	def place_sell(self):
+		coin_balance,fiat_balance = self.get_balances()
+		order_flag = False
+
+		try: 
+			auth_ret = self.auth_client.cancel_all(product_id=self.product_id)
+			initial_price = self.get_bid() + self.quote_increment
+			size = coin_balance
+			if size >= self.min_amount:
+				order_flag = True
+				sell_price = initial_price
+				self.pending_order = True
+				print "Starting Sell Thread"
+				while getattr(self.order_thread,"run", True) and self.sell_flag and size > self.min_amount or len(self.open_orders) > 0:
+					
+					#Check for max slippage and if so do a market buy
+					if (1 - sell_price/initial_price) >= self.max_slippage:
+						self.auth_client.cancel_all(product_id=self.product_id)
+						self.sell(size = size,sell_type = 'market')
+						self.pending_order = False
+						return True
+
+					if len(self.open_orders) > 0: 
+						ret = self.sell(price = sell_price,size = size,sell_type = "limit",partial = 1.0)
+					else: 
+						ret = self.sell(price = sell_price,size = size,sell_type = "limit",partial = .5)
+					
+	               	#Check each order for completion or expiration of 60+ sec
+					self.open_orders = self.get_orders()
+					for order in self.open_orders:
+						order_time = dateutil.parser.parse(order.get('created_at'))
+						dt = datetime.datetime.utcnow().replace(tzinfo = tzutc()) - order_time
+						if dt.total_seconds() > 60: 
+							self.auth_client.cancel_order(order.get('id'))
+
+					time.sleep(2)
+
+					sell_price = self.get_bid() + self.quote_increment
+					coin_balance,fiat_balance = self.get_balances()
+					size = coin_balance
+
+		except Exception as err: 
+			print "Error in place_sell: {}".format(err)
+	       	#Check each order for completion or expiration of 10+ sec
+
+		auth_ret = self.auth_client.cancel_all(product_id=self.product_id)
+		self.pending_order = False
+		self.open_orders = self.get_orders()
+		print "Exiting Sell Thread"
+		return order_flag
+
+	def calc_fiat_balance(self,price,coin_balance,fiat_balance):
+		fiat_balance = fiat_balance
+		coin_value = self.round_fiat(coin_balance*price)
+		equiv_balance = coin_value
+
+		open_orders = self.auth_client.get_orders()
+		if list(open_orders):
+			return 
+
+		self.equivalent_fiat = equiv_balance
+
+	def get_price(self):
+		try:
+			price = self.pc.get_product_ticker(product_id = self.product_id).get('price')
+			if price is None: 
+				time.sleep(2)
+				price = self.pc.get_product_ticker(product_id = self.product_id).get('price')
+				
+			return Decimal(self.pc.get_product_ticker(product_id = self.product_id).get('price'))
+		except Exception as err: 
+			print "ERROR IN GET_PRICE:{}".format(err)
+			return Decimal(0.00)
+
+	def run(self,short_sell_flag = False,short_buy_flag = False,long_buy_flag = False,long_sell_flag = False):
+		fiat_balance = 0.00	
+		coin_balance = 0.00
+		try:
+			price = self.get_price()
+			
+			self.buy_flag = False	#Lets api know that buy occured
+			self.sell_flag = False	#Lets api know that sell occured
+			
+			#self.stop_limit(self.last_buy_price,price,coin_balance)
+
+			#SHORT TERM LOGIC DECISIONS
+			if short_buy_flag and not self.long_flag:
+				print "SHORT BUY"
+				if self.pending_order:
+					self.order_thread.run = False
+					time.sleep(2)
+
+				self.buy_flag = True
+				self.short_flag = True
+				self.order_thread = threading.Thread(target=self.place_buy, name='short_buy_thread')
+				self.order_thread.daemon = True
+				self.order_thread.start()
+				print "after thread running"
+
+
+			if short_sell_flag and not self.long_flag:
+				print "SHORT SELL"
+				if self.pending_order:
+					self.order_thread.run = False
+					#wait for pending order to finish
+					time.sleep(2)
+				self.short_flag = False
+				self.sell_flag = True
+				self.order_thread = threading.Thread(target=self.place_sell, name='short_sell_thread')
+				self.order_thread.daemon = True
+				self.order_thread.start()
+			
+
+			#LONG TERM UPTREND LOGIC DECISIONS
+			if long_buy_flag and not self.short_flag:
+				print "LONG BUY"
+				if self.pending_order:
+					self.order_thread.run = False
+					#wait for pending order to finish
+					time.sleep(2)
+				self.long_flag = True
+				self.buy_flag = True
+				self.order_thread = threading.Thread(target=self.buy, name='long_buy_thread')
+				self.order_thread.daemon = True
+				self.order_thread.start()
+				
+
+
+			if long_sell_flag and not self.short_flag: 
+				print "LONG SELL"
+				if self.pending_order:
+					self.order_thread.run = False
+					#wait for pending order to finish
+					time.sleep(2)
+				self.long_flag = False
+				self.sell_flag = True	
+				self.order_thread = threading.Thread(target=self.sell, name='long_sell_thread')
+				self.order_thread.daemon = True
+				self.order_thread.start()
+				
+
+
+			coin_balance,fiat_balance = self.get_balances()
+			self.calc_fiat_balance(price,coin_balance,fiat_balance)
+			print "COIN_ID: {} EQUIV_FIAT: {} COIN_BALANCE: {} FIAT_BALANCE: {}".format(self.coin_id,self.equivalent_fiat,coin_balance,fiat_balance)
+
+			return self.equivalent_fiat,fiat_balance,coin_balance,price,self.buy_flag,self.sell_flag
+		except Exception as err: 
+			print err
+			return self.equivalent_fiat,fiat_balance,coin_balance,price,self.buy_flag,self.sell_flag
+
+
+if __name__ == '__main__':
+	coin_id = "LTC"
+	product_id = "LTC-USD"
+	bot = gdax_bot(coin_id,product_id)
+	bot.run()
